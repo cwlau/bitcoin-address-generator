880bbe82b0b90173c1b6e6220e1259d390da976c
William CW Lau <contact@cwlau.com>
2021-01-31 02:13:02 +0800

    Correct results

--- src/components/MultiSigAddressForm.tsx
+++ src/components/MultiSigAddressForm.tsx
@@ -45,7 +45,7 @@ export default function MultiSigAddressForm() {
 
     try {
       const pubkeys = pubkeyValues.map((hex: string) => Buffer.from(hex, 'hex'));
-      const { address } = bitcoin.payments.p2wsh({
+      const { address } = bitcoin.payments.p2sh({
         redeem: bitcoin.payments.p2ms({ m: mValue, pubkeys }),
       });
 
@@ -90,16 +90,16 @@ export default function MultiSigAddressForm() {
 
   const setValues = () => {
 
-    setMValue(3);
-    setNValue(4);
+    // Sample from https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/test/integration/addresses.spec.ts#L40
+    setMValue(2);
+    setNValue(3);
     setPubkeyValues([
       '026477115981fe981a6918a6297d9803c4dc04f328f22041bedff886bbc2962e01',
       '02c96db2302d19b43d4c69368babace7854cc84eb9e061cde51cfa77ca4a22b8b9',
-      '023e4740d0ba639e28963f3476157b7cf2fb7c6fdf4254f97099cf8670b505ea59',
       '03c6103b3b83e4a24a0e33a4df246ef11772f9992663db0c35759a5e2ebf68d8e9',
     ]);
 
-    // console.log('setValues done');
+    // Generated address should equal to 36NUkt6FWUi3LAWBqWRdDmdTWbt91Yvfu7
   }
 
   const renderInupts = () => {

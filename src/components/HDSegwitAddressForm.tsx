299cf04710fc687b7aaae8f1dfd77840919df9ae
William CW Lau <contact@cwlau.com>
2021-01-31 02:56:14 +0800

    Backup changes

--- src/components/HDSegwitAddressForm.tsx
+++ src/components/HDSegwitAddressForm.tsx
@@ -1,22 +1,162 @@
 
-// import * as bitcoin from 'bitcoinjs-lib';
-import * as React from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
+import * as bitcoin from 'bitcoinjs-lib';
+import QRCode from 'qrcode';
 
 export default function HDSegwitAddressForm() {
 
-  // const pubkeys = [
-  //   '026477115981fe981a6918a6297d9803c4dc04f328f22041bedff886bbc2962e01',
-  //   '02c96db2302d19b43d4c69368babace7854cc84eb9e061cde51cfa77ca4a22b8b9',
-  //   '023e4740d0ba639e28963f3476157b7cf2fb7c6fdf4254f97099cf8670b505ea59',
-  //   '03c6103b3b83e4a24a0e33a4df246ef11772f9992663db0c35759a5e2ebf68d8e9',
-  // ].map(hex => Buffer.from(hex, 'hex'));
-  // const { address } = bitcoin.payments.p2wsh({
-  //   redeem: bitcoin.payments.p2ms({ m: 3, pubkeys }),
-  // });
+  const [qrcodeUrl, setQRcodeUrl] = useState('');
+  const [seed, setSeed] = useState('');
+  const [path, setPath] = useState('');
+  const [address, setAddress] = useState('');
+  const [resultSuccess, setResultSuccess] = useState(false);
+  const [resultError, setResultError] = useState(false);
+  const [resultErrorMessage, setResultErrorMessage] = useState("");
 
-  // console.log({address});
+  const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+
+  useEffect(() => {
+    if (address === '') {
+      return;
+    }
+
+    QRCode.toDataURL(address)
+      .then(url => {
+        console.log(url);
+        setQRcodeUrl(url);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, [address]);
+
+  const generateAddress = () => {
+    try {
+      // const pubkeys = pubkeyValues.map((hex: string) => Buffer.from(hex, 'hex'));
+      // const { address } = bitcoin.payments.p2sh({
+      //   redeem: bitcoin.payments.p2ms({ m: mValue, pubkeys }),
+      // });
+
+      // // console.log('generateAddress done');
+      // // console.log({ address });
+
+      // if (!address) {
+      //   setAddress('');
+      //   setQRcodeUrl('');
+      //   setResultError(true);
+      //   setResultSuccess(false);
+      //   return;
+      // }
+
+      // setResultError(false);
+      // setResultSuccess(true);
+      // setAddress(address);
+
+    } catch (error) {
+
+      console.warn({error});
+
+      // setAddress('');
+      // setQRcodeUrl('');
+      // setResultError(true);
+      // setResultErrorMessage("Error found with Pubkey #" + (Number(error.__property.replace("pubkeys.", "")) + 1));
+      // setResultSuccess(false);
+      return;
+    }
+
+  }
+
+  const clearResults = () => {
+    setSeed('');
+    setPath('');
+    setResultError(false);
+    setResultSuccess(false);
+    setResultErrorMessage('');
+    setAddress('');
+  }
+
+  const setValues = () => {
+
+    // Sample from https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/test/integration/addresses.spec.ts#L40
+    setSeed('');
+    setPath('');
+
+    // Generated address should equal to 36NUkt6FWUi3LAWBqWRdDmdTWbt91Yvfu7
+  }
+
+
 
   return (
-    <div className="app">Hello from HDSegwitAddressForm</div>
+    <div className="form">
+
+      <div className="container input-container">
+
+        <div className="mb-3 row">
+          <div className="col-12 col-sm-3">
+            <label htmlFor="seed" className="form-label">Seed</label>
+          </div>
+
+          <div className="col-12 col-sm-9">
+            <input type="text" className="form-control" value={seed}
+                id="seed" onChange={
+                  (e: React.ChangeEvent<HTMLInputElement>) => setSeed(e.target.value)
+                }
+              />
+          </div>
+        </div>
+
+        <div className="mb-3 row">
+          <div className="col-12 col-sm-3">
+            <label htmlFor="path" className="form-label">Path</label>
+          </div>
+
+          <div className="col-12 col-sm-9">
+            <input type="text" className="form-control" value={path}
+                id="path" onChange={
+                  (e: React.ChangeEvent<HTMLInputElement>) => setPath(e.target.value)
+                }
+              />
+          </div>
+        </div>
+
+        <hr/>
+
+        <div style={{"float": "right"}}>
+          <input className="btn btn-default" type="button" onClick={setValues} value="Set Sample Values" />
+        </div>
+
+        <div>
+          <input className="btn btn-primary" type="button" onClick={generateAddress} value="Generate Address" />
+          <input className="btn btn-default" type="button" onClick={clearResults} value="Clear Results and Reset Form" />
+        </div>
+
+        <hr/>
+
+        {
+          resultSuccess && <div className="alert alert-success">
+            <h5>The generated Bitcoin Address:</h5>
+            <hr/>
+            <div className="result-container" style={{"textAlign": "center"}}>
+              <div className="result-qrcode-div">{qrcodeUrl && <img src={qrcodeUrl} className="qrcode-img" />}</div>
+              <div className="result-address">{address}</div>
+              <div className="result-address">
+                <a href={ "https://www.blockchain.com/btc/address/" + address } target="blank">View on Blockchain.com</a>
+              </div>
+            </div>
+          </div>
+        }
+
+        {
+          resultError && <div className="alert alert-danger">
+            <h6>Error: Failed to generate Bitcoin Address</h6>
+            <div>
+              Please make sure the inputs are correctly filled. ({resultErrorMessage})
+            </div>
+          </div>
+        }
+
+      </div>
+    </div>
+
   )
 }
